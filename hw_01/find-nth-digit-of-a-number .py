2e3d068b52717f72e343544ef5c53a4867afc7e6
Maxim Stulov <maxim.stulov@n-able.com>
2024-07-08 15:59:13 +0200

    add solution

--- hw_01/find-nth-digit-of-a-number .py
+++ hw_01/find-nth-digit-of-a-number .py
@@ -1,5 +1,19 @@
 arr = [0, -3, 7, 4, 0, 3, 7, 9]
 print(len(arr[2:9]))
 
-num = 10
-print(len(str(num)))
+num_1 = 10
+print(len(str(num_1)))
+
+
+def find_nth(num, ind):
+
+    if ind < 0 or ind >= len(num):
+        return "invalid input index"
+    return num[ind]
+
+
+number = input("number: ")
+index = int(input("index: "))
+result = find_nth(number, index)
+
+print(f"the digit of index {index} is: {result}")
